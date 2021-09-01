5fc776eef06c22643cd141a60e6409e2f153fbd1
muralikrishna25 <muralikrishna.kasturi@openexc.com>
2021-09-01 10:02:06 +0530

    Initial Commit

---
+++ src/main/java/com/google/drive/App.java
@@ -0,0 +1,13 @@
+package com.google.drive;
+
+import org.apache.commons.text.WordUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println(WordUtils.capitalize("hello world!"));
+	}
+}
