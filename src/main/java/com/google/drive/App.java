d2697952da7dd99cb9fc5e42d97358d6b018f590
mkkasturi <mkkasturi12@gmail.com>
2021-09-03 10:03:52 +0530

    Added log4j sample code

--- src/main/java/com/google/drive/App.java
+++ src/main/java/com/google/drive/App.java
@@ -1,14 +1,38 @@
 package com.google.drive;
 
-import org.apache.commons.text.WordUtils;
+import java.sql.Timestamp;
+
+import org.apache.log4j.Logger;
 
 /**
  * Hello world!
  *
  */
 public class App {
+	final static Logger logger = Logger.getLogger(App.class);
+
 	public static void main(String[] args) {
-		System.out.println(WordUtils.capitalize("hello world!"));
-		System.out.println(WordUtils.capitalize("We are feature branch"));
+
+		logger.debug("DEBUG Message");
+		logger.warn("WARN Message");
+		logger.fatal("FATAL Message");
+		logger.error("ERROR Message");
+		logger.trace("TRACE Message");
+
+		while (true) {
+			try {
+				Thread.sleep(5000l);
+				sendNotification();
+				logger.info("Running");
+				System.out.println("Running at " + new Timestamp(System.currentTimeMillis()));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void sendNotification() {
+		logger.info("Notification Sent");
 	}
 }
